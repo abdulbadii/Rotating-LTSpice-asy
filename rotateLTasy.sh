27a250b4c9ca7dc1235c4a233ef6a4b8097733df
abdulbadii <abdulbadii@yandex.com>
2021-09-15 09:24:41 +0000

    init

---
+++ rotateLTasy.sh
@@ -0,0 +1,82 @@
+rotLTSym(){
+local IFS F PR l a x y modP modC mod modW modpt pin pres pfixes res modr;D=${1%/}
+: ${D:=~/Documents/LTspiceXVII/lib/sym}
+if [ -d $D ] ;then pushd $D;n=/*;F=1;PR=~-/; else n=${D%.asy} ;fi
+for fn in $n.asy ;{
+mapfile -t l<"$fn"
+for((i=2;i<${#l[@]};i++)){
+	if [[ ${l[i]} =~ ^((LINE|CIRCLE|ARC|RECTANGLE) Normal )(.+)$'\r'$ ]] ;then #<- newline is \r\n, \n was stripped by mapfile
+		mod=("${mod[@]}" $i "${BASH_REMATCH[1]}" "${BASH_REMATCH[3]}")
+		a=$a\ ${BASH_REMATCH[3]}
+	elif [[ ${l[i]} =~ ^PIN( [0-9]+ [0-9]+)(.+)$'\r'$ ]] ;then
+		modP=("${modP[@]}" $i PIN "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}")
+	elif [[ ${l[i]} =~ ^(WINDOW [0-9] )([0-9]+ [0-9]+)(.+)$'\r'$ ]] ;then
+		modW=("${modW[@]}" $i "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" "${BASH_REMATCH[3]}");fi
+}
+a=($a);for((i=0;i<${#a[@]};i+=2)){
+	x=(${x[@]} ${a[i]});	y=(${y[@]} ${a[i+1]})
+}
+R=${#x[@]};	IFS=$'\n'
+xs=(`sort -n<<<"${x[*]}"`);ys=(`sort -n<<<"${y[*]}"`)
+D=45\ -45
+let Horz=xs[-1]-xs[0]-ys[-1]+ys[0]
+((Horz<0))	&&{ Horz=;D=135\ 45 ;}
+let dx=(xs[0]+xs[-1])/2;let dy=(ys[0]+ys[-1])/2
+unset IFS
+for D in $D ;{
+	d=`bc -l<<<"$D/180*3.14159265358979323844"`
+	cos=`bc -l<<<"c($d)"`
+	sin=`bc -l<<<"s($d)"`
+	minsin=`bc -l<<<"-1*$sin"`
+	rotM=($cos $minsin $sin $cos)
+	for((i=0;i<${#modP[@]};i+=4)){
+		p=(${modP[i+2]})
+		pd=($((p[0]-dx)) $((p[1]-dy)))
+		for((c=0;c<2;c++)){	M=0
+			for((cr=0;cr<2;cr++)){	M=`bc<<<"$M+${pd[cr]}*${rotM[cr*2+c]}"`;}
+			printf -v M %.0f $M; pin[c]=$M
+			((pinfix[c]=((M+${M%%[0-9]*}15)/16)*16))
+		}
+		pres=(${pres[@]} ${pin[@]})
+		pfixes=(${pfixes[@]} ${pinfix[@]})
+		l[modP[i]]=PIN\ ${pinfix[@]}${modP[i+3]}$'\r'
+	}
+	for((i=0;i<${#mod[@]};i+=3)){
+		pts=(${mod[i+2]})
+		unset md
+		for((j=0;j<${#pts[@]};j+=2)){	md=("${md[@]}" $((pts[j]-dx)) $((pts[j+1]-dy)));	}
+		[[ "${mod[i+1]}" == CIR* ]] &&{
+			((${md[0]}==-${md[2]})) && ((${md[1]}==-${md[3]})) &&{	l[mod[i]]=${mod[i+1]}${md[@]}$'\r';continue;}
+		}
+		for((r=0;r<$((${#pts[@]}));r+=2)){
+			for((c=0;c<2;c++)){	M=0
+				for((cr=0;cr<2;cr++)){
+					M=`bc<<<"$M+${md[r+cr]}*${rotM[cr*2+c]}"`;}
+				printf -v modpt[c] %.0f $M
+				modr[r+c]=${modpt[c]}
+			}
+			for((k=0;k<${#pres[@]};k+=2)){
+				[[ "${modpt[@]}" == "${pres[k]} ${pres[k+1]}" ]] &&{	((modr[r]=pfixes[k]));((modr[r+1]=pfixes[k+1]));}
+			}
+		}
+		l[mod[i]]=${mod[i+1]}${modr[@]}$'\r'
+	}
+	for((i=0;i<${#modW[@]};i+=4)){
+		pts=(${modW[i+2]})
+		pts=($((pts[0]-dx)) $((pts[1]-dy)))
+		SD=${modW[i+3]}
+		if [[ "${modW[i+1]}" = *0\  ]] ;then
+			((pts[1]-=5))
+			if((Horz))&&((D==-45))||(((!Horz))&&((D==45))) ;then
+				((pts[0]=pres[0]<pres[2]?pres[0]+5:pres[2]+5))
+			else
+				((pts[0]=pres[0]>pres[2]?pres[0]-3:pres[2]-3))
+				SD=Right${modW[i+3]/Left};	fi
+		else	((pts[1]+=7))
+		fi
+		l[modW[i]]=${modW[i+1]}${pts[@]}$SD$' \r'
+	}
+	((Horz))||((D-=90))
+	for((i=0;i<${#l[@]};i++)){	echo ${l[i]} ;}>$PR${fn%.asy}$D.asy
+};};((F))&&popd
+}
